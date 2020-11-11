9a163687fac0e8ca64b1fe87e91ed0c4029a74c0
Emma Fewer <emmafewer20@gmail.com>
2020-11-11 19:11:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,6 +130,7 @@ end
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |attribute|
+      binding.pry 
       if attribute[:player_name] == player_name
         return attribute[:points]
       end
